a7d5a452a0081dfd4960496fa6fd00d84816d9aa
Jmatra <alanferreira88@gmail.com>
2026-05-08 23:23:39 -0300

    fix: firebase config fallback

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -31,14 +31,12 @@ function assertEnvVars(): void {
 /* ─── Config ─── */
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyA9lboBMRIJZPeDOVIXNuxpO4hmNWzyTGg",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "frutacai-f3423.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "frutacai-f3423",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "frutacai-f3423.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "793258809926",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:793258809926:web:94cb271ff75840a73e0847",};
 
 /* ─── Singleton ─── */
 
